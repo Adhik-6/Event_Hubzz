c484ac617957f16bb47f26dcc773304ca5c43fca
Adhik-6 <adhik.m10a@gmail.com>
2025-04-08 20:43:16 +0530

    added const in axiosInstance

--- client/src/utils/axios.utils.js
+++ client/src/utils/axios.utils.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-baseUrl = (import.meta.env.MODE=="production")?"https://event-hubzz-api.onrender.com/api":`${import.meta.env.VITE_SERVER_URL}${import.meta.env.VITE_PORT}/api`
+const baseUrl = (import.meta.env.MODE=="production")?"https://event-hubzz-api.onrender.com/api":`${import.meta.env.VITE_SERVER_URL}${import.meta.env.VITE_PORT}/api`
 const axiosInstance = axios.create({
   baseURL: baseUrl,
   withCredentials: true,
