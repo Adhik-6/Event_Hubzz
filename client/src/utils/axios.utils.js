8572346821f269edad357948a24d29d50c18a0d5
Adhik-6 <adhik.m10a@gmail.com>
2025-04-08 20:38:29 +0530

    update client

--- client/src/utils/axios.utils.js
+++ client/src/utils/axios.utils.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
-
+baseUrl = (import.meta.env.MODE=="production")?"https://event-hubzz-api.onrender.com/api":`${import.meta.env.VITE_SERVER_URL}${import.meta.env.VITE_PORT}/api`
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_URL}${import.meta.env.VITE_PORT}/api`,
-  // baseURL: "http://localhost:8000/api/v1",
+  baseURL: baseUrl,
   withCredentials: true,
 })
 console.log("reached axios: ", `${import.meta.env.VITE_SERVER_URL}${import.meta.env.VITE_PORT}/api`) // not working
